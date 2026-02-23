b1d681969a3fd8d9b60875969095279242db98a1
dhruvjivani <Dhruvjivani001@gmail.com>
2026-02-23 11:13:32 -0500

    Initial Commit

---
+++ AutoPin Watch App/AutoPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPinApp.swift
+//  AutoPin Watch App
+//
+//  Created by Dhruv Rasikbhai Jivani on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AutoPin_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoPin Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoPin Watch App
+//
+//  Created by Dhruv Rasikbhai Jivani on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
